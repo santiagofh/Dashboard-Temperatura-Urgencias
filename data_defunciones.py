569bda394276fe3949f704bc8e998f3295f80243
santiagofh <santiago.farias@gmail.com>
2025-10-28 12:35:37 -0300

    date def

--- data_defunciones.py
+++ data_defunciones.py
@@ -14,7 +14,7 @@ col=["SEXO",
 ]
 
 df_2024 = pd.read_csv(r'C:\Users\fariass\OneDrive - SUBSECRETARIA DE SALUD PUBLICA\Escritorio\DATA\DEFUNCIONES\DEF2024.csv', sep='|', encoding='LATIN',usecols=col,low_memory=False)
-df_2025 = pd.read_csv(r'C:\Users\fariass\OneDrive - SUBSECRETARIA DE SALUD PUBLICA\Escritorio\DATA\DEFUNCIONES\DEF2025.csv', sep='|', encoding='LATIN',usecols=col,low_memory=False)
+df_2025 = pd.read_csv(r'C:\Users\fariass\OneDrive - SUBSECRETARIA DE SALUD PUBLICA\Escritorio\DATA\DEFUNCIONES\DEF2025.csv', sep=';', encoding='LATIN',usecols=col,low_memory=False)
 #%%
 # Filtrar los datos para la región 13
 filtered_df_2024 = df_2024[df_2024['REG_RES'] == 13]
@@ -24,8 +24,15 @@ filtered_df.loc[filtered_df['EDAD_TIPO'].isin([2, 3, 4]), 'EDAD_CANT'] = 0
 edad_cant_out_of_range = filtered_df[(filtered_df['EDAD_CANT'] >= 80) | (filtered_df['EDAD_CANT'] <= 0)]
 filtered_df['CARDIOVASCULAR'] = filtered_df['DIAG1'].str.startswith('I', na=False)
 filtered_df['DATE'] = pd.to_datetime(filtered_df[['ANO_DEF', 'MES_DEF', 'DIA_DEF']].astype(str).agg('-'.join, axis=1), errors='coerce')
+# Obtener la fecha máxima registrada
+max_date = filtered_df['DATE'].max()
+
+# Filtrar excluyendo la fecha máxima (hoy)
+filtered_df = filtered_df[filtered_df['DATE'] < max_date]
+
+# Guardar el CSV sin el último día
 filtered_df.to_csv(r'data_defunciones/defunciones_2024.csv', index=False, sep='|', encoding='LATIN')
-print("Los datos filtrados y agrupados se han guardado correctamente.")
+print("Los datos filtrados sin el último día se han guardado correctamente.")
 
 # %%
 a=pd.DataFrame({'diag':list((filtered_df['DIAG1'][filtered_df['DIAG1'].str.startswith('I', na=False)].unique()))})
